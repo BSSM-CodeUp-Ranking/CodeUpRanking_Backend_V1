c07901ed38d5123ce6320443ef85b4e35e1fa8a4
J1W0N-1209 <nabook754@naver.com>
2023-07-26 17:11:16 +0900

    ADD :: ranking API

---
+++ ranking_api.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+URL = "https://codeup.kr/ranklist_1.php?school=619"
+
+def rankingApi():
+	ranking = []
+	res = requests.get(URL)
+	html = res.text
+	soup = BeautifulSoup(html)
+
+	for i in range(1,51):
+		rank = int(soup.select_one(f"#ranklist > tbody > tr:nth-child({i}) > td:nth-child({1})").text)
+		id = soup.select_one(f"#ranklist > tbody > tr:nth-child({i}) > td:nth-child({2})").text
+		username = soup.select_one(f"#ranklist > tbody > tr:nth-child({i}) > td:nth-child({3})").text
+		solved = int(soup.select_one(f"#ranklist > tbody > tr:nth-child({i}) > td:nth-child({4})").text)
+		submit = int(soup.select_one(f"#ranklist > tbody > tr:nth-child({i}) > td:nth-child({5})").text)
+
+		ranking.append(
+			{
+				"rank" : rank,
+				"id" : id,
+				"username" : username,
+				"solved" : solved,
+				"submit" : submit
+			}
+		)
+	
+	return json.dumps(ranking,ensure_ascii=False)
